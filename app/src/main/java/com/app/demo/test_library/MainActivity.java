462dafd43e07a63b3228514573a7840612d64c19
Sagar Android Developer <advisefashion@gmail.com>
2023-02-16 16:37:53 +0530

    message

---
+++ app/src/main/java/com/app/demo/test_library/MainActivity.java
@@ -0,0 +1,20 @@
+package com.app.demo.test_library;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public static void Toast_Sp(Context context){
+        Toast.makeText(context, "Test OK SP New Check 4", Toast.LENGTH_SHORT).show();
+    }
+}
